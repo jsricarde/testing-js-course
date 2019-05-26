876992c19e7c472ea8407e8e805122b1c1d0208e
jshricarde <jhormaza@lululemon.com>
2019-05-26 17:30:42 -0500

    jack jack can fly assertion

--- react-jest/shared/other/__tests__/super-heros.js
+++ react-jest/shared/other/__tests__/super-heros.js
@@ -5,6 +5,7 @@ test('returns super heros that can fly', () => {
     console.log('flyingHeros', flyingHeros)
    expect(flyingHeros).toEqual([
     {name: 'Dynaguy', powers: ['disintegration ray', 'fly']},
-    {name: 'Apogee', powers: ['gravity control', 'fly']}
+    {name: 'Apogee', powers: ['gravity control', 'fly']},
+    {name: 'Jack-Jack', powers: ['shapeshifting', 'fly']}
    ])
 })
