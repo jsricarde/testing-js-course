8ee56ef6eac47f8fa16e403f5793649242b6b06b
jshricarde <jhormaza@lululemon.com>
2019-06-04 22:25:30 -0500

    Use react-testing-library to render and test React Components 1.0

--- react-testing/src/__tests__/react-dom.js
+++ react-testing/src/__tests__/react-dom.js
@@ -5,11 +5,18 @@ import {FavoriteNumber} from '../favorite-number'
 // import {queries} from 'dom-testing-library'
 import {getQueriesForElement} from 'dom-testing-library'
 
+function render(ui) {
+  const container = document.createElement('div')
+  ReactDOM.render(ui, container)
+  const queries = getQueriesForElement(container)
+  return {
+    container,
+    ...queries,
+  }
+}
+
 test('renders a number input with a label "Favorite Number"', ()=> {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  const {getByLabelText} = getQueriesForElement(div)
+  const {getByLabelText} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  expect(input).toHaveAttribute('type', 'number')
 })
