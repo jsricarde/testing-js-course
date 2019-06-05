28722a661ab369e89eac3e8e85f962e7052aa517
jshricarde <jhormaza@lululemon.com>
2019-06-04 22:34:21 -0500

    Avoid Memory leaks using react-testing-library’s cleanup function

--- react-testing/src/__tests__/react-dom.js
+++ react-testing/src/__tests__/react-dom.js
@@ -4,11 +4,16 @@ import ReactDOM from 'react-dom'
 import {FavoriteNumber} from '../favorite-number'
 // import {queries} from 'dom-testing-library'
 import {getQueriesForElement} from 'dom-testing-library'
-import {render} from 'react-testing-library'
+import {render, cleanup} from 'react-testing-library'
 
+afterEach(()=>{
+  cleanup()
+  console.log(document.body.outerHTML)
+})
 
 test('renders a number input with a label "Favorite Number"', ()=> {
   const {getByLabelText} = render(<FavoriteNumber />)
+  console.log(document.body.outerHTML)
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
 })
